5dab257fc21249980f8306cb0b0dd7c3fc61a0b7
Pawel Szymanski <pawel.szymanski.2@volvo.com>
2019-04-06 23:46:08 +0200

    reducer and items list for equipment preferences

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import liveData from "./liveDataReducer";
+import equipmentPreferences from "./equipmentPreferencesReducer";
 
-export default combineReducers({ liveData });
+export default combineReducers({ liveData, equipmentPreferences });
