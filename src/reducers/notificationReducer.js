9492c01691b607a3eb1ac6e2792094eed2e98d54
TrzyRazyCztery <3x4to9@gmail.com>
2019-04-16 15:55:51 +0200

    notifications flow

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,55 @@
+import { createActionNamespace } from '../utils/actions';
+
+const registerNotificationAction = createActionNamespace('notification');
+
+export const NOTIFIED_ERROR = registerNotificationAction('NOTIFIED_ERROR');
+export const NOTIFIED_INFO = registerNotificationAction('NOTIFIED_INFO');
+export const NOTIFIED_SUCCESS = registerNotificationAction('NOTIFIED_SUCCESS');
+export const NOTIFIED_WARNING = registerNotificationAction('NOTIFIED_WARNING');
+
+export const getNotification = state => state.notifications.notification;
+
+const initialState = {
+  notification: {},
+};
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case NOTIFIED_ERROR:
+      return {
+        ...state,
+        notification: {
+          type: 'error',
+          message: action.message,
+        },
+      };
+    case NOTIFIED_INFO:
+      return {
+        ...state,
+        notification: {
+          type: 'info',
+          message: action.message,
+        },
+      };
+    case NOTIFIED_SUCCESS:
+      return {
+        ...state,
+        notification: {
+          type: 'success',
+          message: action.message,
+        },
+      };
+    case NOTIFIED_WARNING:
+      return {
+        ...state,
+        notification: {
+          type: 'warning',
+          message: action.message,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
---
+++ src/shared/Notifications.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastr';
+import { connect } from 'react-redux';
+import { getNotification } from '../reducers/notificationReducer';
+
+class Notifications extends React.Component {
+  componentWillReceiveProps(props) {
+    const notification = props.notification;
+    switch (notification.type) {
+      case 'error':
+        this.container.error(notification.message);
+        break;
+      case 'warning':
+        this.container.warning(notification.message);
+        break;
+      case 'info':
+        this.container.info(notification.message);
+        break;
+      case 'success':
+        this.container.success(notification.message);
+        break;
+      default:
+        break;
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ToastContainer
+          ref={container => {
+            this.container = container;
+          }}
+          className="toast-top-right"
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  notification: getNotification(state),
+});
+
+export default connect(mapStateToProps)(Notifications);
